261eb9ed6570ffb64d3322875847f85db489d7b6
Chen-Chen Yeh <ge96noj@mytum.de>
2021-05-19 22:11:56 +0200

    Add commands

---
+++ queries.sql
@@ -0,0 +1,15 @@
+/* select 3 users with highest ID */
+SELECT *
+FROM users
+ORDER BY id DESC
+LIMIT 3;
+/* Join the users and posts table. Show the username of  user id 200 and captoions of all posts they have created*/
+SELECT username, caption
+FROM users 
+JOIN posts ON posts.user_id = users.id
+WHERE users.id = 200;
+/*show each username nad the # of likes they created*/
+SELECT username, COUNT(*)
+FROM users
+JOIN likes ON likes.user_id = users.id
+GROUP BY users.id;;
